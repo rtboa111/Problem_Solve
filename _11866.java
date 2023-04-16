edb8ebea0f1fb3bd3c9e4f77f04b0b1c8ffba4a0
master <laksj113@gmail.com>
2023-04-16 15:29:01 +0900

    use Arrays.binarySearch but timeout

--- _11866.java
+++ _11866.java
@@ -1,120 +1,55 @@
 package Problem_Solve;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _11866 {
 
     public static void main(String[] args) throws IOException {
-        yo<Integer> a = new yo<>();
-        int[] b = a.Execution();
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        for (int i = 0; i < b.length-1; i++) {
-            sb.append(b[i]).append(", ");
-        }
-        sb.append(b[b.length-1]).append(">");
-        System.out.println(sb);
-    }
-}
-
-class Node<E> {
-    public int data;
-    Node<E> next;
-
-    public Node() {
-        this.data = 0;
-        this.next = null;
-    }
-}
-
-class yo<E> {
-
-    public static int N,K;
-    public static int size;
-
-    public static void readIn() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-    }
-
-    Node<E> head;
-    Node<E> tail;
-
-    public yo() throws IOException {
-        this.head = null;
-        this.tail = null;
-        readIn();
-        CreateYo();
-    }
-
-    public int[] Execution() {
-
-        int[] result = new int[N];
-        if (N == 1) {
-            result[0] = 1;
-            return result;
+        int[] cardN = new int[N];
+        for (int i = 0; i < N; i++) {
+            cardN[i] = Integer.parseInt(st.nextToken());
         }
-        int j = 0;
-        Node<E> x = head;
-        while (j < N-1) {
-            for (int i = 0; i < K-1; i++) {
-                x = x.next;
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] cardM = new int[M];
+        for (int i = 0; i <M; i++) {
+            cardM[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(cardN);
+        StringBuilder sb= new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int index = Arrays.binarySearch(cardN,cardM[i]);
+            if (index<0) {
+                sb.append(0).append(" ");
+                continue;
             }
-            result[j] = x.data;
-            x = remove(x);
-            j++;
-        }
-        result[j] = head.data;
-        return result;
-    }
-
-    private Node<E> remove(Node<E> x) {
-
-        if (size == 1) {
-            return head;
-        }
-
-        Node<E> y = head;
-
-        while (!y.next.equals(x)) {
-            y = y.next;
-        }
-        if (x.equals(head)) {
-            head = x.next;
-        }
-        if (x.equals(tail)) {
-            tail = y;
-        }
-        y.next = null;
-        y.next = x.next;
-        x.data = 0;
-        x.next = null;
-        size--;
-        return y.next;
-    }
-
-    private void CreateYo() {
-
-        Node<E> x = new Node<E>();
-        size = N;
-        x.data = 1;
-        head = x;
-        if (N == 1) {
-            tail = x;
-            return;
-        }
-        for (int i = 2; i <= N; i++) {
-            Node<E> y = new Node<E>();
-            y.data = i;
-            x.next = y;
-            x = x.next;
-            if (i == N) {
-                tail = x;
-                x.next = head;
+            int count = 1;
+            int next = index+1;
+            while (next<N) {
+                if (cardN[index]==cardN[next]) {
+                    next++;
+                    count++;
+                } else {
+                    break;
+                }
             }
+            int prev = index-1;
+            while (prev>=0) {
+                if (cardN[index]==cardN[prev]) {
+                    prev--;
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            sb.append(count).append(" ");
         }
+        sb.deleteCharAt(sb.length()-1);
+        System.out.println(sb);
     }
 }
