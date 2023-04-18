8378071425958794514a09fb8d51d0e5eabf2cd3
master <laksj113@gmail.com>
2023-04-18 21:58:48 +0900

    10816 for binarySearch function study that upper, lower bound complete

--- _10816.java
+++ _10816.java
@@ -1,83 +1,65 @@
 package Problem_Solve;
 
-
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+/*
+    중복값이 있을 경우 binarySearch 의 upper bound 와
+    lower bound 활용
+ */
 public class _10816 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] cardN = new int[N];
         for (int i = 0; i < N; i++) {
             cardN[i] = Integer.parseInt(st.nextToken());
         }
-        M = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         int[] cardM = new int[M];
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i <M; i++) {
             cardM[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(cardN);
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb= new StringBuilder();
         for (int i = 0; i < M; i++) {
-            value = cardM[i];
-            sb.append(Duplication(cardN)).append(" ");
+            int index = binarySearchLower(cardN, cardM[i]);
+            int index2 = binarySearchUpper(cardN, cardM[i]);
+            sb.append(index2-index).append(" ");
         }
+        sb.deleteCharAt(sb.length()-1);
         System.out.println(sb);
     }
-    public static int value,N,M;
+    public static int binarySearchLower(int[] a, int b) {
 
-    public static int BSearch(int[] arr) {
-
-        int index = -1;
         int start = 0;
-        int end = arr.length-1;
-        int mid = (start+end)/2;
-
-        while (start!=end) {
-            if (arr[mid]==value) {
-                index = mid;
-                return index;
-            } else if (arr[mid] < value) {
-                start = mid;
-                mid = (start+end)/2 + 1;
+        int end = a.length;
+        while (start<end) {
+            int mid = (start+end)/2;
+            if (a[mid] >= b) {
+                end = mid;
             } else {
-                end = mid-1;
-                mid = (start+end)/2 + 1;
+                start = mid+1;
             }
         }
-        if (arr[start]==value) {
-            index = start;
-        }
-        return index;
+        return start;
     }
-    public static int Duplication(int[] arr) {
 
-        int index = BSearch(arr);
-        if (index==-1) {
-            return 0;
+    public static int binarySearchUpper(int[] a, int b) {
+
+        int start = 0;
+        int end = a.length;
+        while (start<end) {
+            int mid = (start+end)/2;
+            if (a[mid] > b) {
+                end = mid;
+            } else {
+                start = mid+1;
+            }
         }
-        int count = 1;
-        int next = index+1;
-        while (next<N) {
-            if (arr[index]==arr[next]) {
-                next++;
-                count++;
-            } else break;
-        }
-        int prev = index-1;
-        while (prev>=0) {
-            if (arr[index]==arr[prev]) {
-                prev--;
-                count++;
-            } else break;
-        }
-        return count;
+        return start;
     }
 }
-
