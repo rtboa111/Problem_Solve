bfc2037ecec9fd70bd91e5fd24fe054a682ee1e5
master <laksj113@gmail.com>
2023-04-27 07:24:57 +0900

    27433 factorial function implement by recursive function"

--- _27433.java
+++ _27433.java
@@ -1,2 +1,28 @@
-package Problem_Solve;public class _27433 {
+package Problem_Solve;
+
+import java.io.*;
+/*
+ * 팩토리얼
+ *
+ * 재귀함수로 구현한 팩토리얼 함수
+ */
+public class _27433 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(factorial(N,N));
+        }
+    }
+    public static long factorial(long N,long X) {
+        if (N == 1) {
+            return X;
+        }
+        N--;
+        return factorial(N,X*N);
+    }
 }
+
