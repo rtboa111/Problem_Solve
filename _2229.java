3e6246a857f70fcc042e075ae89f1e76e9cf9146
master <laksj113@gmail.com>
2023-06-11 00:58:21 +0900

    by dp

--- _2229.java
+++ _2229.java
@@ -1,2 +1,26 @@
-package Problem_Solve;public class _2229 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _2229 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+1];
+        int[] arr = new int[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            int min = Integer.MAX_VALUE, max = 0;
+            for (int j = i; j >= 1; j--) {
+                min = Math.min(min, arr[j]);
+                max = Math.max(max, arr[j]);
+                dp[i] = Math.max(dp[i], dp[j-1] + max - min);
+            }
+        }
+        System.out.println(dp[N]);
+    }
 }
--- _9251.java
+++ _9251.java
@@ -1,2 +1,25 @@
-package Problem_Solve;public class _9251 {
+package Problem_Solve;
+
+import java.io.*;
+
+public class _9251 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr1 = br.readLine().toCharArray();
+        char[] arr2 = br.readLine().toCharArray();
+        int[][] dp = new int[arr1.length+1][arr2.length+1];
+
+        for (int i = 1; i <= arr1.length; i++) {
+            for (int j = 1; j <= arr2.length; j++) {
+                if (arr1[i-1] == arr2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        System.out.println(dp[arr1.length][arr2.length]);
+    }
 }
