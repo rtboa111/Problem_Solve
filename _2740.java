075a19746c1fbc58458dd47efdce9adc31fa9efb
master <laksj113@gmail.com>
2023-06-13 00:41:42 +0900

    by matrix multiply

--- _2740.java
+++ _2740.java
@@ -1,2 +1,50 @@
-package Problem_Solve;public class _2740 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _2740 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] B = new int[M][K];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] res = new int[N][K];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                for (int k = 0; k < M; k++) {
+                    res[i][j] += (A[i][k] * B[k][j]);
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                bw.write(res[i][j]+" ");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
 }
