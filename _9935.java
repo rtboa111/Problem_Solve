ca083361269b95a765d9e3859c4a0300712c06f0
master <laksj113@gmail.com>
2023-06-14 23:27:45 +0900

    by String handling

--- _9935.java
+++ _9935.java
@@ -1,2 +1,43 @@
-package Problem_Solve;public class _9935 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+import java.util.Deque;
+
+public class _9935 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[] string = br.readLine().toCharArray();
+        char[] boom = br.readLine().toCharArray();
+        Deque<Character> d = new LinkedList<>();
+        for (int i = 0; i < string.length; i++) {
+            char cur = string[i];
+            d.add(cur);
+            int boomLen = boom.length-1;
+            while (!d.isEmpty() && d.peekLast() == boom[boomLen]) {
+                Deque<Character> q = new LinkedList<>();
+                while (!d.isEmpty() && boomLen >= 0 && d.peekLast() == boom[boomLen]) {
+                    q.addFirst(d.removeLast());
+                    boomLen--;
+                }
+                if (boomLen >= 0) {
+                    d.addAll(q);
+                    break;
+                }
+                boomLen = boom.length-1;
+            }
+        }
+
+        if (d.isEmpty()) {
+            bw.write("FRULA");;
+        } else {
+            for (char s : d) {
+                bw.write(s+"");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
 }
