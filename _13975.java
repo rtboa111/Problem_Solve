825bbbf793168e42b6df0d2c048e348e707f54d7
master <laksj113@gmail.com>
2023-06-16 01:13:29 +0900

    by priority queue

--- _13975.java
+++ _13975.java
@@ -1,2 +1,32 @@
-package Problem_Solve;public class _13975 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _13975 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            PriorityQueue<Long> pq = new PriorityQueue<>(N+1);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                pq.add(Long.parseLong(st.nextToken()));
+            }
+            long res = 0;
+            while (pq.size() > 1) {
+                long cur = pq.poll();
+                cur += pq.poll();
+                res += cur;
+                pq.add(cur);
+            }
+            bw.write(res+"");
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
 }
