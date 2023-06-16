a7388f55e413bb5241b49b61b27d374ed34d1326
master <laksj113@gmail.com>
2023-06-16 23:30:16 +0900

    by dp

--- _26437.java
+++ _26437.java
@@ -1,2 +1,31 @@
-package Problem_Solve;public class _26437 {
+package Problem_Solve;
+
+import java.io.*;
+
+public class _26437 {
+
+    static final int max = 100001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        int[] dp = new int[max];
+        for (int i = 1; i < max; i++) {
+            dp[i] = i;
+        }
+        for (int i = 2; i < max ; i++) {
+            dp[i] = Math.min(dp[i], dp[i-1]+1);
+            for (int j = 2; i * j < max; j++) {
+                dp[i*j] = Math.min(dp[i*j], dp[i] + 4 + (2 * (j-1)));
+            }
+        }
+        for (int t = 1; t <= T; t++) {
+            int L = Integer.parseInt(br.readLine());
+            bw.write("Case #" + t + ": " + dp[L]);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
 }
