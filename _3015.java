0da5104eb8171824b389d757a24140326796adb2
master <laksj113@gmail.com>
2023-06-22 00:37:27 +0900

    by stack

--- _3015.java
+++ _3015.java
@@ -1,2 +1,33 @@
-package Problem_Solve;public class _3015 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.Stack;
+
+public class _3015 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] line = new long[N];
+        for (int i = 0; i < N; i++) {
+            line[i] = Long.parseLong(br.readLine());
+        }
+        Stack<long[]> stack = new Stack<>();
+
+        long res = 0;
+        for (int i = N-1; i >= 0; i--) {
+            long cur = line[i];
+            long cnt = 0;
+            while (!stack.isEmpty() && cur >= stack.peek()[0]) {
+                if (cur == stack.peek()[0]) {
+                    cnt += stack.pop()[1] + 1;
+                } else {
+                    res += stack.pop()[1] + 1;
+                }
+            }
+            res += (stack.isEmpty()) ? cnt : cnt + 1;
+            stack.add(new long[]{cur, cnt});
+        }
+        System.out.println(res);
+    }
 }
