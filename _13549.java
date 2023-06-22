87ae4055f496fd0b2b83d5fb3806b8da8f47f58f
master <laksj113@gmail.com>
2023-06-23 00:45:04 +0900

    by dijkstra

--- _13549.java
+++ _13549.java
@@ -1,2 +1,72 @@
-package Problem_Solve;public class _13549 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _13549 {
+    static class Node implements Comparable<Node> {
+        int end, cost;
+        public Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return cost - o.cost;
+        }
+    }
+    static int n, k;
+    static final int MAX = 100000;
+    static ArrayList<ArrayList<Node>> path;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        path = new ArrayList<>(MAX+1);
+        for (int i = 0; i <= MAX; i++) {
+            path.add(new ArrayList<>());
+        }
+        path.get(0).add(new Node(1, 1));
+
+        for (int i = 1; i < MAX; i++) {
+            path.get(i).add(new Node(i-1, 1));
+            path.get(i).add(new Node(i+1, 1));
+            if (i << 1 <= MAX) {
+                path.get(i).add(new Node(i << 1, 0));
+            }
+        }
+        path.get(MAX).add(new Node(MAX-1, 1));
+
+        System.out.println(dijkstra());
+    }
+    static int dijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(n, 0));
+        int[] dist = new int[MAX+1];
+        Arrays.fill(dist, MAX);
+        dist[n] = 0;
+
+        while(!pq.isEmpty()) {
+            Node tmp = pq.remove();
+            int cur = tmp.end;
+
+            if (dist[cur] < tmp.cost) {
+                continue;
+            }
+            if (cur == k) {
+                break;
+            }
+
+            for (Node n : path.get(cur)) {
+                if (dist[n.end] > dist[cur] + n.cost) {
+                    dist[n.end] = dist[cur] + n.cost;
+                    pq.add(new Node(n.end, dist[n.end]));
+                }
+            }
+        }
+        return dist[k];
+    }
 }
--- _1753.java
+++ _1753.java
@@ -1,2 +1,76 @@
-package Problem_Solve;public class _1753 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _1753 {
+    static class Node implements Comparable<Node> {
+        int end, cost;
+        public Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return cost - o.cost;
+        }
+    }
+    static int v, e;
+    static final int INF = (1 << 30);
+    static ArrayList<ArrayList<Node>> graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine())-1;
+        graph = new ArrayList<>(v);
+        for (int i = 0; i < v; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken())-1;
+            int e = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken());
+            graph.get(s).add(new Node(e, c));
+        }
+
+        for (int i : dijkstra(start)) {
+            if (i == INF) {
+                bw.write("INF");
+            } else {
+                bw.write(i+"");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+    static int[] dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        int[] dist = new int[v];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        while(!pq.isEmpty()) {
+            Node tmp = pq.remove();
+            int cur = tmp.end;
+
+            if (dist[cur] < tmp.cost) {
+                continue;
+            }
+
+            for (Node n : graph.get(cur)) {
+                if (dist[n.end] > dist[cur] + n.cost) {
+                    dist[n.end] = dist[cur] + n.cost;
+                    pq.add(new Node(n.end, dist[n.end]));
+                }
+            }
+        }
+        return dist;
+    }
 }
