b7fdfc278bbbc04e3bf63da4f9bb28c7f45010ff
master <laksj113@gmail.com>
2023-06-24 01:09:31 +0900

    by floyd-warshall

--- _11404.java
+++ _11404.java
@@ -1,2 +1,54 @@
-package Problem_Solve;public class _11404 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _11404 {
+    static final long MAX = (1L << 40);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        long[][] graph = new long[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    graph[i][j] = 0;
+                } else {
+                    graph[i][j] = MAX;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken())-1;
+            int e = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken());
+            graph[s][e] = Math.min(graph[s][e], c);
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (long j : graph[i]) {
+                if (j == MAX) {
+                    bw.write(0+" ");
+                } else {
+                    bw.write(j + " ");
+                }
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
 }
--- _1956.java
+++ _1956.java
@@ -1,2 +1,53 @@
-package Problem_Solve;public class _1956 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _1956 {
+    static final long MAX = (1L << 40);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        long[][] graph = new long[V][V];
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (i == j) {
+                    graph[i][j] = 0;
+                } else {
+                    graph[i][j] = MAX;
+                }
+            }
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken())-1;
+            int e = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken());
+            graph[s][e] = c;
+        }
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+
+        long min = MAX;
+
+        for (int i = 0; i < V; i++) {
+            for (int j = i+1; j < V; j++) {
+                if (graph[i][j] != MAX && graph[j][i] != MAX) {
+                    min = Math.min(min, graph[i][j] + graph[j][i]);
+                }
+            }
+        }
+
+        System.out.println((min == MAX) ? -1 : min);
+    }
 }
