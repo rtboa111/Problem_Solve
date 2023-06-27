b00347d741d47131e9ddf7924a7f9cc28bded2e4
master <laksj113@gmail.com>
2023-06-28 00:26:07 +0900

    by manacher

--- _14444.java
+++ _14444.java
@@ -1,2 +1,43 @@
-package Problem_Solve;public class _14444 {
+package Problem_Solve;
+
+import java.io.*;
+
+public class _14444 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(manacherCorrection(br.readLine().toCharArray()));
+    }
+    static int manacherCorrection(char[] s) {
+        StringBuilder sb = new StringBuilder();
+        int size = s.length;
+        for (int i = 0; i < size; i++) {
+            sb.append('@').append(s[i]);
+        }
+        sb.append('@');
+
+        s = sb.toString().toCharArray();
+        size = s.length;
+
+        int R = 0;
+        for(int i = 1; i < size; i++) {
+            int r = R+1;
+            if (i-r >= 0 && i+r < size) {
+                while(r >= 0 && s[i-r] == s[i+r]) {
+                    r--;
+                }
+            } else {
+                break;
+            }
+            if (r < 0) {
+                r = R + 2;
+                while(i-r >= 0 && i+r < s.length && s[i-r] == s[i+r]) {
+                    r++;
+                }
+                R = r-1;
+            }
+        }
+        return R;
+    }
 }
--- _14444_2.java
+++ _14444_2.java
@@ -1,2 +1,43 @@
-package Problem_Solve;public class _14444_2 {
+package Problem_Solve;
+
+import java.io.*;
+
+public class _14444_2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(manacher(br.readLine().toCharArray()));
+    }
+    static int manacher(char[] s) {
+        StringBuilder sb = new StringBuilder();
+        int size = s.length;
+        for (int i = 0; i < size; i++) {
+            sb.append('@').append(s[i]);
+        }
+        sb.append('@');
+
+        s = sb.toString().toCharArray();
+        size = s.length;
+        int[] A = new int[s.length];
+
+        int r = 0, p = 0, max = 0;
+        for(int i = 0; i < size; i++) {
+            if (i <= r) {
+                A[i] = Math.min(A[p*2 - i], r - i);
+            } else {
+                A[i] = 0;
+            }
+            while(i-A[i] > 0 && i+A[i]+1 < s.length && s[i-A[i]-1] == s[i+A[i]+1]) {
+                A[i]++;
+            }
+            if (r < i + A[i]) {
+                r = i + A[i];
+                p = i;
+            }
+            max = Math.max(max, A[i]);
+        }
+
+        return max;
+    }
 }
