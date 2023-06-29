38cd439dc82fd2da2940dcd64cf7b2725891898c
master <laksj113@gmail.com>
2023-06-30 00:42:26 +0900

    by dfs

--- _16953.java
+++ _16953.java
@@ -1,2 +1,41 @@
-package Problem_Solve;public class _16953 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _16953 {
+    static final int MAX = 1000000000;
+    static HashMap<Long, Integer> map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        map = new HashMap<>(1000000);
+        map.put((long) A, 0);
+        dfs(A, 0);
+        if (map.containsKey((long) B)) {
+            System.out.println(map.get((long) B)+1);
+        } else {
+            System.out.println(-1);
+        }
+    }
+    static void dfs(long cur, int cnt) {
+        if (cur > MAX) {
+            return;
+        }
+        map.put(cur, cnt);
+
+        if (map.getOrDefault(cur*2, MAX) > cnt + 1) {
+            dfs(cur*2, cnt +1);
+        }
+
+        String s = String.valueOf(cur);
+        s += "1";
+        cur = Long.parseLong(s);
+        if (map.getOrDefault(cur, MAX) > cnt + 1) {
+            dfs(cur, cnt +1);
+        }
+    }
 }
--- _1987.java
+++ _1987.java
@@ -1,2 +1,43 @@
-package Problem_Solve;public class _1987 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _1987 {
+
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static char[][] board;
+    static boolean[] check;
+    static int R, C, res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            board[i] = br.readLine().toCharArray();
+        }
+        check = new boolean[26];
+        check[board[0][0] - 'A'] = true;
+        dfs(0, 0, 1);
+        System.out.println(res);
+    }
+    static void dfs(int x, int y, int cnt) {
+        res = Math.max(res, cnt);
+
+        for (int j = 0; j < 4; j++) {
+            x += dx[j]; y += dy[j];
+
+            if (x < R && x >= 0 && y < C && y >= 0 && !check[board[x][y] - 'A']) {
+                check[board[x][y] - 'A'] = true;
+                dfs(x, y, cnt + 1);
+                check[board[x][y] - 'A'] = false;
+            }
+
+            x -= dx[j]; y -= dy[j];
+        }
+    }
 }
