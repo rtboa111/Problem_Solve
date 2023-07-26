32286a49a83eb1a35f812d7eefc81ebbbc6a3943
master <laksj113@gmail.com>
2023-07-26 15:01:27 +0900

    by dp

--- _2011.java
+++ _2011.java
@@ -1,2 +1,39 @@
-package Problem_Solve;public class _2011 {
+package Problem_Solve;
+
+import java.io.*;
+
+public class _2011 {
+
+    static final int MOD = 1000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        int len = num.length();
+        int[] dp = new int[len+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        if (num.charAt(0) == '0') {
+            System.out.println(0);
+            return;
+        }
+
+        for(int i = 2; i <= len; i++) {
+            String cur = num.substring(i-2, i);
+            int curNum = Integer.parseInt(cur);
+            if ((cur.charAt(0) > '2' && cur.charAt(1) == '0') || curNum == 0) {
+                dp[len] = 0;
+                break;
+            }
+            if (curNum >= 10 && curNum <= 26) {
+                dp[i] += dp[i-2];
+            }
+            if (cur.charAt(1) != '0') {
+                dp[i] += dp[i-1];
+            }
+            dp[i] %= MOD;
+        }
+        System.out.println(dp[len]);
+    }
 }
