39de7a8357e18fb3ae42dc99fa972ad98c7471d0
master <laksj113@gmail.com>
2023-07-28 00:19:29 +0900

    by union find

--- _9372.java
+++ _9372.java
@@ -1,2 +1,52 @@
-package Problem_Solve;public class _9372 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _9372 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            parents = new int[N+1];
+            for (int j = 0; j < N; j++) {
+                parents[j] = j;
+            }
+            int cnt = 0;
+            for (int j = 0; j < M; j++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                if (union(a, b)) {
+                    cnt++;
+                }
+            }
+            bw.write(String.valueOf(cnt));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+    static int[] parents;
+    static int find(int x) {
+        if (parents[x] == x) {
+            return x;
+        }
+        return parents[x] = find(parents[x]);
+    }
+    static boolean union(int a, int b) {
+        int A = find(a);
+        int B = find(b);
+
+        if (A == B) return false;
+
+        parents[B] = A;
+        return true;
+    }
 }
