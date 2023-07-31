aaa3b618cc2a8ddacee1d99b6377f51b2879bdb4
master <laksj113@gmail.com>
2023-07-31 23:56:41 +0900

    by sorting

--- Progermmers/crossing_the_stepping_stone.java
+++ Progermmers/crossing_the_stepping_stone.java
@@ -1,9 +1,38 @@
 package Problem_Solve.Progermmers;
-import java.io.*;
+
+import java.util.ArrayList;
 
 public class crossing_the_stepping_stone {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public int solution(int[] stones, int k) {
+        int len = stones.length;
+        ArrayList<int[]> arr = new ArrayList<>(len);
+        for(int i = 0; i < len; i++) {
+            arr.add(new int[]{stones[i], i});
+        }
+        arr.sort((o1, o2) -> o1[0] - o2[0]);
+
+        int n = 0;
+        int cnt = 0;
+        for(int[] cur : arr) {
+            n += cur[0] - n;
+            cnt++;
+            if(cnt < k) {
+                continue;
+            }
+            int i = cur[1]+1;
+            int j = cur[1]-1;
+            while(i < len && stones[i] <= n) {
+                i++;
+            }
+            while(j >= 0 && stones[j] <= n) {
+                j--;
+            }
+            if(i - j - 1 >= k) {
+                break;
+            }
+        }
+
+        return n;
     }
 }
