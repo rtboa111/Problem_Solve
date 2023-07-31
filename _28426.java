a58ab2fa249fcb70b9ca9a3a72545867b38b9489
master <laksj113@gmail.com>
2023-07-31 23:56:22 +0900

    by constructive

--- _28426.java
+++ _28426.java
@@ -1,9 +1,17 @@
 package Problem_Solve;
+
 import java.io.*;
 
 public class _28426 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        sb.append(3).append(" ");
+        for (int i = 2, cnt = 0; cnt < n-1; i+=2, cnt++) {
+            sb.append(i * 3).append(" ");
+        }
+        System.out.println(sb);
     }
 }
