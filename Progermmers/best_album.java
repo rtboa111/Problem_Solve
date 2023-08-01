a3dbe3c97e192ce7b4eba93c692366e03e7cd198
master <laksj113@gmail.com>
2023-08-01 17:42:34 +0900

    by hash map + sorting

--- Progermmers/best_album.java
+++ Progermmers/best_album.java
@@ -1,9 +1,62 @@
 package Problem_Solve.Progermmers;
-import java.io.*;
+
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class best_album {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    class Node implements Comparable<Node> {
+        String g;
+        int total;
+        ArrayList<int[]> idx;
+
+        public Node(String g) {
+            this.idx = new ArrayList<>(101);
+            this.total = 0;
+            this.g = g;
+        }
+
+        public void add(int num) {
+            total += num;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return o.total - total;
+        }
+    }
+
+    public int[] solution(String[] genres, int[] plays) {
+
+        HashMap<String, Integer> index = new HashMap<>(100 * 4);
+        ArrayList<Node> total = new ArrayList<>(100);
+        int x = 0;
+        for(String s : genres) {
+            if(!index.containsKey(s)) {
+                index.put(s, index.size());
+                total.add(new Node(s));
+            }
+            Node tmp = total.get(index.get(s));
+            tmp.add(plays[x]);
+            tmp.idx.add(new int[]{plays[x], x++});
+        }
+        total.sort(Node::compareTo);
+
+        int len = total.size();
+        ArrayList<Integer> arr = new ArrayList<>(len);
+        for(int i = 0; i < len; i++) {
+            ArrayList<int[]> cur = total.get(i).idx;
+            cur.sort((o1, o2) -> o2[0] - o1[0]);
+            for(int j = 0; j < Math.min(cur.size(), 2); j++) {
+                arr.add(cur.get(j)[1]);
+            }
+        }
+
+        int[] ans = new int[arr.size()];
+        int idx = 0;
+        for(int i : arr) {
+            ans[idx++] = i;
+        }
+        return ans;
     }
 }
