574bd7d752f12166ad6edcd3040f825212fa8c66
master <laksj113@gmail.com>
2023-08-04 21:22:18 +0900

    by hash map + stack

--- Progermmers/babbling_2.java
+++ Progermmers/babbling_2.java
@@ -1,9 +1,43 @@
 package Problem_Solve.Progermmers;
-import java.io.*;
+
+import java.util.HashMap;
+import java.util.Stack;
 
 public class babbling_2 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public int solution(String[] babbling) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("aya", 1);
+        map.put("ye", 2);
+        map.put("woo", 3);
+        map.put("ma", 4);
+
+        int res = 0;
+        loop:
+        for (String s : babbling) {
+            int len = s.length();
+            int idx = 0;
+            int hi = 0;
+            Stack<Integer> stack = new Stack<>();
+            stack.add(0);
+            while (idx < len) {
+                int cur = 0;
+                if (s.charAt(idx) == 'a' || s.charAt(idx) == 'w') {
+                    hi = idx + 3;
+                } else {
+                    hi = idx + 2;
+                }
+                if (hi > len) continue loop;
+                cur = map.getOrDefault(s.substring(idx, hi), 0);
+                if (cur != 0 && cur != stack.peek()) {
+                    stack.add(cur);
+                } else {
+                    continue loop;
+                }
+                idx = hi;
+            }
+            res++;
+        }
+        return res;
     }
 }
