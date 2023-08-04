eea35a98417a7a193f70c0862ba660890e23cef4
master <laksj113@gmail.com>
2023-08-04 21:29:50 +0900

    by math

--- _2292.java
+++ _2292.java
@@ -1,9 +1,24 @@
 package Problem_Solve;
+
 import java.io.*;
 
 public class _2292 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; ;i++) {
+            if (N==1) {
+                System.out.println(1);
+                return;
+            }
+            // a0 = 1, a1 = 7, a2 = 19, a3 = 37 ...
+            // an = (6*n+6)*n/2 + 1 이고
+            // ai가 N보다 커지는 지점이 N이 가능해 지는 범위 이다
+            if (N<=((6*i+6)*i/2)+1) {
+                System.out.println(i+1);
+                return;
+            }
+        }
     }
 }
