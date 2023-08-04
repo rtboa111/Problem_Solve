9a597eb3650d57410bbcb1806ad2b880c72f18b1
master <laksj113@gmail.com>
2023-08-04 21:21:51 +0900

    by sieve of eratosthenes + dfs

--- Progermmers/find_prime_number.java
+++ Progermmers/find_prime_number.java
@@ -1,9 +1,44 @@
 package Problem_Solve.Progermmers;
-import java.io.*;
 
 public class find_prime_number {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static char[] numbers;
+    static int len, res;
+    static final int MAX = 10000000;
+    static boolean[] num, check;
+
+    public int solution(String Numbers) {
+        numbers = Numbers.toCharArray();
+        len = numbers.length;
+        num = new boolean[MAX];
+        num[0] = true;
+        num[1] = true;
+        for(int i = 2; i * i < MAX; i++) {
+            if(num[i]) continue;
+            for(int j = i * i; j < MAX; j += i) {
+                num[j] = true;
+            }
+        }
+        res = 0;
+        check = new boolean[len];
+        dfs(0);
+        return res;
+    }
+
+    static void dfs(int n) {
+        if(!num[n]) {
+            num[n] = true;
+            res++;
+        }
+
+        for(int i = 0; i < len; i++) {
+            if(!check[i] && (n != 0 || numbers[i] != '0')) {
+                check[i] = true;
+                n = n*10 + (numbers[i] - '0');
+                dfs(n);
+                check[i] = false;
+                n /= 10;
+            }
+        }
     }
 }
