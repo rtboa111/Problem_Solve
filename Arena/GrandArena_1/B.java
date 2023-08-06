bb782aa753f13e2f6c761dfaec69667915bc341a
master <laksj113@gmail.com>
2023-08-06 21:18:23 +0900

    by math

---
+++ Arena/GrandArena_1/B.java
@@ -0,0 +1,25 @@
+package Problem_Solve.Arena.GrandArena_1;
+
+import java.io.*;
+
+public class B {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] check = new int[10];
+        int res = -1;
+        for (int i = 0; i < 5; i++) {
+            int cur = Integer.parseInt(br.readLine());
+            check[cur]++;
+        }
+        int idx = 0;
+        for (int i : check) {
+            if (i %2 != 0) {
+                System.out.println(idx);
+                return;
+            }
+            idx++;
+        }
+    }
+}
