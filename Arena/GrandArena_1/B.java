542bd17af90eafd7efb4e9b634e4cc3ee3aa69b7
master <laksj113@gmail.com>
2023-08-06 21:18:54 +0900

    by math

--- Arena/GrandArena_1/B.java
+++ Arena/GrandArena_1/B.java
@@ -6,16 +6,14 @@ public class B {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int[] check = new int[10];
-        int res = -1;
         for (int i = 0; i < 5; i++) {
             int cur = Integer.parseInt(br.readLine());
             check[cur]++;
         }
         int idx = 0;
         for (int i : check) {
-            if (i %2 != 0) {
+            if (i % 2 != 0) {
                 System.out.println(idx);
                 return;
             }
