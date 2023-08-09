0163bda9384cdff4c9b42a625b2721f1d06f9917
master <laksj113@gmail.com>
2023-08-09 14:24:56 +0900

    by dp

--- _25633.java
+++ _25633.java
@@ -1,9 +1,33 @@
 package Problem_Solve;
+
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class _25633 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[][] dp = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            dp[i][1] = arr[i];
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+                if (dp[i-1][j-1] >= arr[i]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1] + arr[i]);
+                }
+            }
+        }
+        for (int i = n; i >= 1; i--) {
+            if (dp[n][i] != 0) {
+                System.out.println(i);
+                return;
+            }
+        }
     }
 }
