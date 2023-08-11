a6e56b351837d3d72d915751a714bc26c0082c64
master <laksj113@gmail.com>
2023-08-11 14:38:33 +0900

    by greedy

--- Progermmers/maze_escape_command.java
+++ Progermmers/maze_escape_command.java
@@ -1,9 +1,51 @@
 package Problem_Solve.Progermmers;
-import java.io.*;
 
 public class maze_escape_command {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        if(dist(x, y, r, c) % 2 != k % 2 || k < dist(x, y, r, c)) {
+            return "impossible";
+        }
+        String res = "";
+        while(x < n && k > dist(x, y, r, c)) {
+            k--;
+            x++;
+            res += 'd';
+        }
+        while(y > 1 && k > dist(x, y, r, c)) {
+            k--;
+            y--;
+            res += 'l';
+        }
+        while(k > dist(x, y, r, c)) {
+            k -= 2;
+            res += 'r';
+            res += 'l';
+        }
+        if(x < r) {
+            for(int i = (r - x); i > 0; i--) {
+                res += 'd';
+            }
+        }
+        if(y > c) {
+            for(int i = (y - c); i > 0; i--) {
+                res += 'l';
+            }
+        }
+        if(y < c) {
+            for(int i = (c - y); i > 0; i--) {
+                res += 'r';
+            }
+        }
+        if(x > r) {
+            for(int i = (x - r); i > 0; i--) {
+                res += 'u';
+            }
+        }
+
+        return res;
+    }
+    static int dist(int x, int y, int r, int c) {
+        return Math.abs(x - r) + Math.abs(y - c);
     }
 }
