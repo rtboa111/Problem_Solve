1ede8a0a4207d8ccb2dcaf60fa23c75cc972965e
master <laksj113@gmail.com>
2023-08-14 13:10:28 +0900

    by implements

--- Arena/GrandArena_2/B.java
+++ Arena/GrandArena_2/B.java
@@ -1,9 +1,37 @@
 package Problem_Solve.Arena.GrandArena_2;
+
 import java.io.*;
 
 public class B {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        String s3 = br.readLine();
+        int res = 0;
+        if (s1.charAt(0) != 'F' && s1.charAt(0) != 'B') {
+            res = Integer.parseInt(s1) + 3;
+        } else if (s2.charAt(0) != 'F' && s2.charAt(0) != 'B') {
+            res = Integer.parseInt(s2) + 2;
+        } else if (s3.charAt(0) != 'F' && s3.charAt(0) != 'B') {
+            res = Integer.parseInt(s3) + 1;
+        }
+        String ans = "";
+        boolean flag = false;
+        if (res % 3 == 0) {
+            ans += "Fizz";
+            flag = true;
+        }
+        if (res % 5 == 0) {
+            ans += "Buzz";
+            flag = true;
+        }
+
+        if (flag) {
+            System.out.println(ans);
+        } else {
+            System.out.println(res);
+        }
     }
 }
