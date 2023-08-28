14a4e8a69b7d43eb71bae0f430fd217939c11d09
master <laksj113@gmail.com>
2023-08-28 10:59:26 +0900

    by ccw

--- _11758.java
+++ _11758.java
@@ -1,1 +1,27 @@
-package Problem_Solve;public class _11758 {
+package Problem_Solve;
+
+import java.io.*;
+import java.util.*;
+
+public class _11758 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[] cordi = new int[6];
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            cordi[i] = x; cordi[i+3] = y;
+        }
+        System.out.println(ccw(cordi[0], cordi[3], cordi[1], cordi[4], cordi[2], cordi[5]));
+    }
+    static int ccw(long x1, long y1, long x2, long y2, long x3, long y3) {
+        long ans = x1*y2 + x2*y3 + x3*y1;
+        ans -= x2*y1 + x3*y2 + x1*y3;
+        if (ans > 0) return 1;
+        if (ans < 0) return -1;
+        return 0;
+    }
+}
--- _2166.java
+++ _2166.java
@@ -1,9 +1,31 @@
 package Problem_Solve;
+
 import java.io.*;
+import java.util.*;
 
 public class _2166 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long x0 = Integer.parseInt(st.nextToken());
+        long y0 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        long[] prev = new long[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        double res = 0;
+        for (int i = 2; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            long x = Integer.parseInt(st.nextToken());
+            long y = Integer.parseInt(st.nextToken());
+            res += ccw(x0, y0, prev[0], prev[1], x, y);
+            prev[0] = x; prev[1] = y;
+        }
+        System.out.printf("%.1f", Math.abs(res));
+    }
+    static double ccw(long x1, long y1, long x2, long y2, long x3, long y3) {
+        double ans = x1*y2 + x2*y3 + x3*y1;
+        ans -= x2*y1 + x3*y2 + x1*y3;
+        return ans / 2;
     }
 }
