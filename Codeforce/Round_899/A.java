9a3822a21c5ac389b14add54e55f54caca843af8
master <laksj113@gmail.com>
2023-09-26 22:26:52 +0900

    by math

--- Codeforce/Round_899/A.java
+++ Codeforce/Round_899/A.java
@@ -1,1 +1,30 @@
-package Problem_Solve.Codeforce.Round_899;public class A {
+package Problem_Solve.Codeforce.Round_899;
+
+import java.io.*;
+import java.util.*;
+
+public class A {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int cur = 1;
+            for (int j = 0; j < n; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num == cur) cur++;
+                if (j == n-1) {
+                    continue;
+                }
+                cur++;
+            }
+            bw.write(String.valueOf(cur));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
